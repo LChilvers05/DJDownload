44e779d958b42f324ca99bcff1529760c4192f45
Lee Chilvers <lee.chilvers@icloud.com>
2026-01-10 00:26:49 +0000

    begin audio splitting

--- src/main.py
+++ src/main.py
@@ -1,12 +1,46 @@
 from repository import Repository
+from model.playlist import Playlist
+from model.track import Track
 
 def main():
-    fileName = "src/playlists/Ipizza.xml"
-    playlistName = "Ipizza"
+    create_track_files(
+        playlist_file="src/playlists/Ipizza.xml",
+        audio_file="src/audio/Ipizza.wav",
+        output_dir="../output/"
+    )
 
-    playlist = Repository().get_playlist(fileName, playlistName)
-    print(playlist.tracks[0].title, playlist.tracks[0].artist, playlist.tracks[0].duration)
-    
+
+def create_track_files(playlist_file, audio_file, output_dir):
+    repo = Repository()
+
+    # playlist = repo.get_playlist(playlist_file)
+    playlist = Playlist(
+        name="Ipizza",
+        tracks=[
+            Track(title="Track 1", artist="Artist A", duration=5500),
+            Track(title="Track 2", artist="Artist B", duration=5500),
+            Track(title="Track 3", artist="Artist C", duration=5000),
+        ]
+    )
+    print(f"Fetched playlist '{playlist.name}' with {len(playlist.tracks)} tracks")
+
+    audio = repo.get_audio(audio_file)
+    print(f"Loaded raw audio from '{audio_file}'")
+
+    tracks = split_audio(audio, playlist)
+    repo.save_tracks(tracks, playlist.tracks, output_dir)
+
+
+def split_audio(raw_audio, playlist):
+    audios = []
+    start = 0
+    for track in playlist.tracks:
+        end = start + track.duration
+        audio = raw_audio[start:end]
+        audios.append(audio)
+        start = end
+
+    return audios
 
 if __name__ == "__main__":
     main()
--- src/repository.py
+++ src/repository.py
@@ -1,24 +1,65 @@
 import plistlib
+from pydub import AudioSegment
+from pathlib import Path
+from mutagen.aiff import AIFF
+from mutagen.id3 import TIT2, TPE1, TALB, TRCK, TDRC
 from model.track import Track
 from model.playlist import Playlist
 
 class Repository:
 
-    def get_playlist(self, fileName, playlistName):
-        file = self.__open_file(fileName)
+    def get_audio(self, file_name):
+        return AudioSegment.from_wav(file_name)
+    
+
+    def get_playlist(self, file_name):
+        name = file_name.split('/')[-1].split('.xml')[0]
+        file = self.__open_file(file_name)
         file_tracks = self.__get_tracks_in_file(file)
-        file_playlist = self.__find_playlist_in_file(file, playlistName)
-        playlist = self.__construct_playlist(file_playlist, file_tracks, playlistName)
+        file_playlist = self.__find_playlist_in_file(file, name)
+        playlist = self.__construct_playlist(file_playlist, file_tracks, name)
         return playlist
+
+
+    def save_tracks(self, tracks, metadata, directory):
+        path = Path(directory)
+        path.mkdir(exist_ok=True)
+        for track, meta in zip(tracks, metadata):
+            self.__save_track(track, meta, path)
+
+    
+    def __save_track(self, track: AudioSegment, metadata: Track, path):
+        track_path = path / f"{metadata.title}.mp3"
+
+        print(track.sample_width)
+        print(track.frame_rate)
+        print(track.channels)
+        track.set_channels(2)
+        track.set_sample_width(4)
+
+        track.export(out_f=metadata.title, format="mp3")
+        # self.__add_track_metadata(track_path, metadata)
+        print(f"Saved track '{metadata.title}'")
+
+    
+    # def __add_track_metadata(self, path, metadata: Track):
+    #     aiff = AIFF(path)
+    #     if aiff.tags is None:
+    #         aiff.add_tags()
+
+    #     aiff.tags.add(TIT2(encoding=3, text=metadata.title))
+    #     aiff.tags.add(TPE1(encoding=3, text=metadata.artist))
+
+    #     aiff.save()
             
 
-    def __construct_playlist(self, playlist, tracks, playlistName):
+    def __construct_playlist(self, playlist, tracks, playlist_name):
         playlist_tracks = []
         for item in playlist.get("Playlist Items", []):
             id = item.get("Track ID")
             info = tracks.get(str(id), {})
             if not info:
-                raise ValueError(f"Track with ID '{id}' not found in playlist '{playlistName}'")
+                raise ValueError(f"Track with ID '{id}' not found in playlist '{playlist_name}'")
             
             playlist_tracks.append(
                 Track(
@@ -28,23 +69,23 @@ class Repository:
                 )
             )
         
-        return Playlist(name=playlistName, tracks=playlist_tracks)
+        return Playlist(name=playlist_name, tracks=playlist_tracks)
     
 
     def __get_tracks_in_file(self, file):
         return file.get("Tracks", {})
     
     
-    def __find_playlist_in_file(self, file, playlistName):
+    def __find_playlist_in_file(self, file, playlist_name):
         playlists = file.get("Playlists", [])
         
         for playlist in playlists:
-            if playlist.get("Name") == playlistName:
+            if playlist.get("Name") == playlist_name:
                 return playlist
         
-        raise ValueError(f"Playlist '{playlistName}' not found in file")
+        raise ValueError(f"Playlist '{playlist_name}' not found in file")
 
           
-    def __open_file(self, fileName):
-        with open(fileName, "rb") as fp:
+    def __open_file(self, file_name):
+        with open(file_name, "rb") as fp:
             return plistlib.load(fp)
